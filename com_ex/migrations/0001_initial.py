22ad4dd775b8fd05e963416012bdde6a359cff0c
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-03-10 14:13:59 +0200

    created models(People, Document), migration, initial_data

---
+++ com_ex/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('education', models.CharField(max_length=250)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='People',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=250)),
+                ('last_name', models.CharField(max_length=250)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='document',
+            name='people',
+            field=models.ForeignKey(to='com_ex.People'),
+        ),
+    ]
--- com_ex/models.py
+++ com_ex/models.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class People(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+
+class Document(models.Model):
+    education = models.CharField(max_length=250)
+    people = models.ForeignKey(People)
